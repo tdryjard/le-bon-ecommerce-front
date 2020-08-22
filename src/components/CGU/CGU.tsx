2b5d4753cd233a9d007a0671e41dfe44a05ae4e5
Thomas.dryjard <newwbuiss@gmail.com>
2020-08-22 20:40:16 +0200

    correction fautes

--- src/components/CGU/CGU.tsx
+++ src/components/CGU/CGU.tsx
@@ -8,16 +8,16 @@ export const CGU = () => {
                 <h4 className="title">PRÉAMBULE</h4><br /><br />
                 <p className="text">
                     le-bon-ecommerce.fr est une plateforme de développement et déploiement de site web spécialisé dans l’édition de boutique en ligne pour particulier ou professionnels.
-            Plateforme développé par Thomas Dryjard Des Garniers n° de SIRET 85372114000012, numéro de télpéhone: 0676323576, adresse de domiciliation : 12 rue marc dequoy, 41000 Blois, France<br /><br />
+            Plateforme développée par Thomas Dryjard Des Garniers n° de SIRET 85372114000012, numéro de téléphone: 0676323576, adresse de domiciliation : 12 rue marc dequoy, 41000 Blois, France<br /><br />
 
-            La réalisation des site web sont pris en charge principalement par Thomas Dryjard Des Garniers et d’autres développeur web professionnels en sous-traitance.<br/>
+            La réalisation des sites web sont pris en charge principalement par Thomas Dryjard Des Garniers et d’autres développeurs web professionnels en sous-traitance.<br/>
             Les présentes Conditions générales ont pour objet de définir les modalités de création d’utilisation des sites web développés par les différents tiers du bon-ecommerce ainsi que les futurs possesseurs de ces sites web.<br /><br />
             le-bon-ecommerce.fr se réserve le droit, à tout moment et à sa seule discrétion, de modifier ou mettre à jour les présentes Conditions, en publiant sur le Site et/ou par envoi d’une notification par email tout changement, ajout ou mise à jour, dans la mesure du possible 30 jours au moins avant son entrée en vigueur. Ces Conditions d’Utilisation régissent la relation entre “le-bon-ecommerce.fr”, “Nous”, “Thomas Dryjard Des Garniers” et Vous, dénommé "L'utilisateur" ou "Le Client" ou "Vous" y compris si vous représentez un employeur ou un client.
             Les conditions particulières éventuellement négociées entre le-bon-ecommerce.fr et l’Utilisateur prévalent sur les présentes Conditions générales.
             le-bon-ecommerce Vous concède un droit d’accès et d’utilisation non exclusif, nominatif, personnel et temporaire. Cette concession ne saurait être considérée comme une cession au sens du Code de la propriété intellectuelle d’un quelconque droit de propriété intellectuelle sur le Service le-bon-ecommerce.fr
         </p><br /><br /><br /><br />
                 <h4 className="title">La conception de votre boutique en ligne</h4><br /><br />
-                <p className="text">Tous les sites que nous réalisons sont développé avec les langages et technologies suivantes :<br /><br />
+                <p className="text">Tous les sites que nous réalisons sont développés avec les langages et technologies suivantes :<br /><br />
         Javascript<br />
         ReactJs<br />
         NodeJs<br />
@@ -25,18 +25,18 @@ export const CGU = () => {
         Le déploiement de vos sites est réalisé via les plateformes suivantes :<br /><br />
         Netlify.com<br />
         Heroku.com<br /><br />
-        Nous mettons en place le maximum de normes en terme de sécurités afin de protéger la base de données et le serveur de votre site via des vérifications Json Web Token, vérification d'autorisation de cookies et Cors.
-        Marlgrés les différentes normes de sécurités mises en places, nous ne pouvons garantire une protection inviolable de votre serveur et base de données si une personnes malveillante porte atteintes aux données de votre sites ou si vous partagez vos identifiants d’aministration de votre site, le-bon-ecommerce.fr ainsi que toutes les personnes travaillants à la réalisation de vos sites, ne pourront pas être tenues responsables.
+        Nous mettons en place le maximum de normes en termes de sécurités afin de protéger la base de données et le serveur de votre site via des vérifications Json Web Token, vérification d'autorisation de cookies et Cors.
+        Marlgré les différentes normes de sécurité mises en places, nous ne pouvons garantir une protection inviolable de votre serveur et base de données si une personne malveillante porte atteinte aux données de votre site ou si vous partagez vos identifiants d’administration de votre site, le-bon-ecommerce.fr ainsi que toutes les personnes travaillant à la réalisation de vos sites, ne pourront pas être tenues responsables.
         </p><br /><br /><br /><br />
                 <h4 className="title">UTILISATIONS INTERDITES</h4><br /><br />
                 <p className="text">
-                    Une fois votre site livré via un mail vous fournissant vos identifiants administrateur, votre boutique en ligne est votre possession et il sera de votre charge d’assumer vos engagements envers vos clients et le respect des lois en terme de vente sur le web.
+                    Une fois votre site livré via un mail vous fournissant vos identifiants administrateurs, votre boutique en ligne est votre possession et il sera de votre charge d’assumer vos engagements envers vos clients et le respect des lois en matière de vente sur le web.
             Nous ne pourrons pas être tenus responsable en cas d’utilisation frauduleuse de votre site e-commerce développé par un tiers de le-bon-ecommerce.fr.<br /><br />
-            Vos clés d’acceptation de paiement Stripe.com sont entré dans vos variable de base de données par nos soins en suivant l’exactitudes des informations fournies par vous lors de la commande de votre site.
-            En cas d’erreur de votre part lors de la transmissions des informations lors de la commande de votre site, vous pouvez nous contacter afin que nous corrigeons gratuitement ces informations dans un délais de 14 jours si celles-ci concernent vos clés API Stripe.com.<br /><br />
-            La transmissions des bonnes informations est de votre responsabilité, cela est également valable pour les conditions générales et vente et conditions d’utilisateurs de votre site que VOUS NOUS transmettez lors du renseignement de vos informations.
+            Vos clés d’acceptation de paiement Stripe.com sont entrées dans vos variables de base de données par nos soins en suivant l’exactitude des informations fournies par vous lors de la commande de votre site.
+            En cas d’erreur de votre part lors de la transmission des informations lors de la commande de votre site, vous pouvez nous contacter afin que nous corrigions gratuitement ces informations dans un délai de 14 jours si celles-ci concernent vos clés API Stripe.com.<br /><br />
+            La transmission des bonnes informations est de votre responsabilité, cela est également valable pour les conditions générales et de ventes et conditions d’utilisateurs de votre site que VOUS NOUS transmettez lors du renseignement de vos informations.
 
-            En cas de manquement ou d’erreurs dans les mentions légales de votre sites, celles-ci seront e votre responsabilité.
+            En cas de manquement ou d’erreurs dans les mentions légales de votre site, celles-ci seront e votre responsabilité.
         </p><br /><br /><br /><br />
                 <h4 className="title">RESPECT DE LA RÉGLEMENTATION APPLICABLE</h4><br /><br />
                 <p className="text">
@@ -49,15 +49,15 @@ export const CGU = () => {
                 <h4 className="title">DROITS DE PROPRIÉTÉ INTELLECTUELLE ET INDUSTRIELLE DE LE-BON-ECOMMERCE</h4><br /><br />
                 <p className="text">
                     Tous les programmes, les services, les processus, les designs, les logiciels, les technologies, les marques et les noms commerciaux, les inventions figurant sur le Site, accessible via le Site ou via les Services de le-bon-ecommerce.fr sont la propriété de Thomas Dryjard Des Garniers.
-            Le contenus fournis par vous lors du renseignement des informations à la commande de votre sites ne font pas partis de la propriété intellectuel de le-bon-ecommerce.fr<br /><br />
+            Le contenus fournis par vous lors du renseignement des informations à la commande de votre site ne font pas partis de la propriété intellectuelle de le-bon-ecommerce.fr<br /><br />
 
             Le code source, le nom de domaine ainsi que le design de vos sites font eux partis de notre propriété intellectuel, cela est nécessaire afin de prendre en charge du début jusqu’à la fin le développement et le déploiement de votre site ainsi que pour limiter la copie de notre activité et de nos sites développés.
         </p><br /><br /><br /><br />
                 <h4 className="title">PROTECTION DES DONNÉES PERSONNELLES DES TIERS</h4><br /><br />
                 <p className="text">
-                    Le-bon-ecommerce.fr a accès aux informations fournis lors de la commande d’une boutique en ligne, si celle-ci arrive à son terme, ainsi qu’aux informations de paiement mis à part vos données bancaires.<br/>
+                    Le-bon-ecommerce.fr a accès aux informations fournies lors de la commande d’une boutique en ligne, si celle-ci arrive à son terme, ainsi qu’aux informations de paiement mis à part vos données bancaires.<br/>
                     Ces données sont stockées sur des serveurs sécurisés et font l’objet d’un traitement informatique afin de fournir la prestation souscrite par l’Utilisateur.
-            le-bon-ecommerce.fr s’engage à ne pas céder ni louer lces données.<br /><br />
+            le-bon-ecommerce.fr s’engage à ne pas céder ni louer les données.<br /><br />
             La divulgation à des tiers des données à caractère personnel ne pourra intervenir que sur autorisation de l’Utilisateur attestant que le titulaire des données à caractère personnel a lui-même autorisé cette divulgation ou sur demande des autorités légalement compétentes, sur réquisition judiciaire, ou dans le cadre d’un contentieux judiciaire.
         </p>
             </div>
